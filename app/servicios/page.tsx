efbaf158224814c212c09e680aeada6dea851ec2
manuel17 <manuel17Co>
2025-09-20 23:43:34 -0500

    Obtener imagen por enlace (Funciones propias de next)

--- app/servicios/page.tsx
+++ app/servicios/page.tsx
@@ -1,20 +1,19 @@
 // app/services/page.tsx
 import Image from "next/image";
-import servicesImage from "../../app/assets/services.avif"; 
 
 export default function ServiciosPage() {
   return (
     <div className="relative w-full h-[70vh]">
-      {/* Imagen de fondo */}
+      {/* Imagen de fondo desde enlace externo */}
       <Image
-        src={servicesImage}
+        src="https://static.wixstatic.com/media/820831_4badbe8289e54f299867565203d031e6~mv2.jpg/v1/fill/w_1143,h_900,al_c,q_85,usm_0.33_1.00_0.00,enc_avif,quality_auto/820831_4badbe8289e54f299867565203d031e6~mv2.jpg"
         alt="Servicios del hotel"
         fill
         priority
         className="object-cover"
       />
 
-      {/* Overlay oscuro para mejorar contraste */}
+      {/* Overlay oscuro */}
       <div className="absolute inset-0 bg-black bg-opacity-30"></div>
 
       {/* Texto centrado */}
