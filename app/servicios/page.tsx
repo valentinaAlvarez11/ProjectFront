865d8eab503a488c9b68a935a435522d0ccfbc9c
manuel17 <manuel17Co>
2025-09-20 21:25:04 -0500

    Implementacion del header global en la seccion services

---
+++ app/servicios/page.tsx
@@ -0,0 +1,11 @@
+import Header from '../../components/organisms/HeaderComponent';
+
+export default function ServiciosPage() {
+  return (
+    <>
+      <Header />
+      {/* Contenido de la página de servicios */}
+      <h1>Servicios</h1>
+    </>
+  );
+}
